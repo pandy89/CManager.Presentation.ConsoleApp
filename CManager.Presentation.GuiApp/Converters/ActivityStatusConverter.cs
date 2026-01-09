fa3b5cdf22a5ed06114be5144e52fb636dc3bf28
Maria Magnell <maria.magnell@gmail.com>
2026-01-09 17:29:18 +0100

    Simple WPF from the letture "WPF - Skapa en enkel applikation med WPF Grafisk applikationsutveckling".

---
+++ CManager.Presentation.GuiApp/Converters/ActivityStatusConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CManager.Presentation.GuiApp.Converters;
+
+public class ActivityStatusConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isCompleted)
+        {
+            return isCompleted ? "Completed" : "Ongoing";
+        }
+        return "Unknown";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
