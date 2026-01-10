62d674d710fdec8030c7ee9488b51b5a3707274c
Maria Magnell <maria.magnell@gmail.com>
2026-01-10 15:02:21 +0100

    Navigation in WPF with MVVM and CommunityToolKit.

---
+++ CManager.Presentation.GuiApp/ViewModels/AddActivityViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CManager.Presentation.GuiApp.ViewModels;
+
+public partial class AddActivityViewModel : ObservableObject
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    [ObservableProperty]
+    private string _title = "Add New Activity";
+
+    [RelayCommand]
+    private void GoToActivities()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _serviceProvider.GetRequiredService<ActivitiesViewModel>();
+    }
+    public AddActivityViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+}
